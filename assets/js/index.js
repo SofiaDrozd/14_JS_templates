3a98071508cf68701544068e4a8f0e405e4672b5
SofiaDrozd <drozdsofia2005@gmail.com>
2024-11-01 16:34:42 -0900

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,106 @@
+// 1)
+
+// let currentNumber = 1;
+// const TOTAL_NUMBERS = 10;
+
+// while (currentNumber <= TOTAL_NUMBERS) {
+//     console.log(currentNumber*currentNumber);
+//     currentNumber++;
+// }
+
+
+// 2)
+
+// const PASSWORD_PATTERN = 'sqrt';
+
+// let currentTry = 1;
+// let password = '';
+
+// let isPasswordCorrect = false;
+
+// do {
+//     isPasswordCorrect = prompt('Введіть пароль') === PASSWORD_PATTERN;
+// } while (++currentTry  && !isPasswordCorrect);
+
+// if (isPasswordCorrect) {
+//     console.log('Ви успішно увійшли в систему')
+// }
+
+// 3)
+
+// let sum = 0;
+
+// for (let i = 1; i <=100; i++) {
+//     sum += i;
+// }
+// console.log(sum); 
+
+// 4)
+
+// let currentNumber = 10;
+// const TOTAL_NUMBERS = 50;
+
+// while (currentNumber <= TOTAL_NUMBERS ) {
+//     if (currentNumber%5===0) {
+//         console.log(currentNumber);
+//     }
+//     currentNumber++;
+// }
+
+// 5)
+
+// let size = prompt('Введіть висоту трикутника:');
+// let triangle = '';
+
+// for (let i = 1; i <= size; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         triangle += '*';
+//     }
+//     triangle += '\n';
+// }
+
+// console.log(triangle);
+
+
+// 6) 
+
+// let size = 6;
+// let square = "";
+
+// for (let i = 0; i < size; i++) {
+//     for (let j = 0; j < size; j++) {
+//         if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i === j) {
+//             square += "*";
+//         } else {
+//             square += " ";
+//         }
+//     }
+//     square += "\n";  
+// }
+
+// console.log(square);
+
+// 7)
+
+// let size = 6;
+// let square = "";
+
+// for (let i = 0; i < size; i++) {
+//     for (let j = 0; j < size; j++) {
+//         if (i === 0 || i === size - 1 || j === 0 || j === size - 1 || i + j === size - 1) {
+//             square += "*";
+//         } else {
+//             square += " ";
+//         }
+//     }
+//     square += "\n";  
+// }
+
+// console.log(square);
+
+
+
+
+
+
+
